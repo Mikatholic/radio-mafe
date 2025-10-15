9f69527d48872d842ed1f61aba4dce71bedff130
Mikatholic <miguelangelferroescalante@gmail.com>
2025-10-14 19:42:00 -0500

    Avance de la clase MisFavoritos.java

--- src/com/aluracursos/radiomafe/modelos/MisFavoritos.java
+++ src/com/aluracursos/radiomafe/modelos/MisFavoritos.java
@@ -1,11 +1,11 @@
 package com.aluracursos.radiomafe.modelos;
 
 public class MisFavoritos {
-    void incluye(Audio audio){
+    public void incluye(Audio audio){
         if(audio.getClasificacion() >= 8){
-            System.out.println(audio.getTitulo() + "Es uno de los favoritos actuales");
+            System.out.println(audio.getTitulo() + " Es uno de los favoritos actuales");
         } else {
-            System.out.println(audio.getTitulo() + "Tambien es uno de los favoritos");
+            System.out.println(audio.getTitulo() + " Tambien es uno de los favoritos");
         }
     }
 }
